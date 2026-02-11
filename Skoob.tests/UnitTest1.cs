7066e39c3869e6ad2c9295f8f1f5ae34fd439bbd
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-02-11 18:44:57 -0300

    tests/initial config

--- Skoob.tests/UnitTest1.cs
+++ Skoob.tests/UnitTest1.cs
@@ -1,15 +1,58 @@
-﻿namespace Skoob.tests;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Skoob.DTOs;
+namespace Skoob.tests;
 
-public class Tests
+[TestFixture]
+public class UpdatePasswordDTOTests
 {
-    [SetUp]
-    public void Setup()
+    private IList<ValidationResult> ValidateModel(object model)
     {
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(model);
+        
+        Validator.TryValidateObject(model, validationContext, validationResults, true);
+        
+        return validationResults;
     }
 
     [Test]
-    public void Test1()
+    public void Should_Fail_When_Confirmation_Does_Not_Match_NewPassword()
     {
-        Assert.Pass();
+        var dto = new UpdatePasswordDTO
+        {
+            OldPassword = "OldPassword123",
+            NewPassword = "NewStrongPassword123",
+            ConfirmNewPassword = "WRONG_PASSWORD" 
+        };
+
+        var errors = ValidateModel(dto);
+
+        
+        Assert.That(errors, Is.Not.Empty, "Deveria ter erros de validação");
+        
+        var hasMismatchError = errors.Any(e => e.ErrorMessage.Contains("As senhas não conferem"));
+        Assert.That(hasMismatchError, Is.True, "O erro deveria ser sobre senhas não conferirem");
     }
-}
+
+    [Test]
+    public void Should_Pass_When_Passwords_Match()
+    {
+     
+        var dto = new UpdatePasswordDTO
+        {
+            OldPassword = "OldPassword123",
+            NewPassword = "NewStrongPassword123",
+            ConfirmNewPassword = "NewStrongPassword123" 
+        };
+
+ 
+        var errors = ValidateModel(dto);
+
+    
+        Assert.That(errors, Is.Empty, "Não deveria ter nenhum erro de validação");
+    }
+
+    
+}   
